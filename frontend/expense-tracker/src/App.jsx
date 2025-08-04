11570fa77c5463c449f1776aeb22f59b26d84ab3
Duy Anh <duyanhdao297@gmail.com>
2025-08-04 16:12:32 +0700

    feat(frontend): define routes for login, signup, dashboard

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+
 const App = () => {
-   return <div>App</div>;
+   return (
+      <div>
+         <Router>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/signup" element={<SignUp />} />
+               <Route path="/dashboard" element={<Home />} />
+            </Routes>
+         </Router>
+      </div>
+   );
 };
 
 export default App;
